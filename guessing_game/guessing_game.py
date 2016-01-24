7cd75c18a5b7b692b28485254c8941b88cea5742
Jabez Abraham <jabezabraham@yahoo.com>
2016-01-23 21:22:08 -0600

    Initial commit to add default .gitIgnore and .gitAttribute files.

---
+++ guessing_game/guessing_game.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import random
+
+'''
+Simple guessing game for trying out 
+number of guesses before 
+reaching the answer
+
+Python version: 3.4
+'''
+
+x = 0; count = 0
+# x = int(input("Please enter a number between 1 to 20: "))
+val = random.randrange(1,10)
+
+while x != val:
+    try:
+        x = int(input("Please enter a number between 1 to 10: "))
+    except:
+        print ("Something went wrong with your input, please try again")
+        continue
+    if x < val:
+        print ("You guessed smaller")
+    elif x > val:
+        print ("you guessed higher")
+    count += 1
+    
+
+ 
+print ("Good Job, you guessed right: ", x, "and you took {0} times to guess it".format(count))
